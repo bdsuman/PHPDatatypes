37103a27919147a52070839991fb02b4b6855970
Suman Sen <mesuman@yahoo.com>
2022-03-24 12:21:09 +0600

    finding php datatype on case study

---
+++ README.md
@@ -0,0 +1,10 @@
+### Topics of PHP Datatypes 
+1. Case Study:
+	I have three old hp monitors, not new. I want to sell it on my own website.
+    That’s why I should create a product upload form on my website,
+    then upload my product with specification and available quantity.
+    My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes,
+    I will sell it for 8000 tk.
+
+### For More Help
+Suman Sen E-mail: mesuman@yahoo.com
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Class 2 Assingment: Suman Chandra Sen, Id:1886 
+
+Case Study:
+	I have three old hp monitors, not new. I want to sell it on my own website.
+    That’s why I should create a product upload form on my website,
+    then upload my product with specification and available quantity.
+    My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes,
+    I will sell it for 8000 tk.
+*/
+
+//Monitor Details
+$brand_name="HP"; //Datatype is String
+$type="Old"; //Datatype is String
+$quantity=3; //Datatype is Integer
+$size=15.6; //Datatype is Double/Float
+$has_vga_cable_status= true; //Datatype is Bolean 
+$price=8000; //Datatype is Integer
+
+
+?>
